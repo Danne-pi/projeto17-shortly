04b59897184e0664ce4054a5652a5b981304dc95
= <=>
2023-03-03 04:41:55 -0300

    kk

--- src/database/db.js
+++ src/database/db.js
@@ -2,16 +2,18 @@ import pg from 'pg';
 import dotenv from "dotenv";
 
 dotenv.config();
-
-pg.types.setTypeParser(20, function(val) {
-    return parseInt(val)
-})
-pg.types.setTypeParser(1700, function(val) {
-    return parseFloat(val)
-})
-
 const {Pool} = pg;
+
+// pg.types.setTypeParser(20, function(val) {
+//     return parseInt(val)
+// })
+// pg.types.setTypeParser(1700, function(val) {
+//     return parseFloat(val)
+// })
+
 
 export const connection = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
+
+// sudo su -c "pg_dump <nome-da-sua-database> --inserts --no-owner" postgres > dump.sql
